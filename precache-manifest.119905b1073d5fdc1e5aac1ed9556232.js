627cec9e8905704d1223cb92ba299117792f7764
afaferz <afa00.papa@gmail.com>
2022-03-02 18:28:21 -0300

    08d7297

---
+++ precache-manifest.119905b1073d5fdc1e5aac1ed9556232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18492bff06711735c84",
+    "url": "/css/app.37c6d8f7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3cfe3ff4f1c5c1a2606926b3b9b456b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96dbc3c8c522e9aa7b93",
+    "url": "/js/about.e870ff70.js"
+  },
+  {
+    "revision": "b18492bff06711735c84",
+    "url": "/js/app.1449671a.js"
+  },
+  {
+    "revision": "1d464d8f2469b7905b51",
+    "url": "/js/chunk-vendors.ef99743e.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
